0ced88c1dc0f0c664fe52a90795940f5b48452fd
lilwi22 <110235434+lilwi22@users.noreply.github.com>
2022-11-27 19:08:06 +0100

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,11 @@
+/* let numButtonClicks = 0;
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("button").textContent =
+        "Button Clicked times: " + numButtonClicks;
+} */
+
+function handleEvent() {
+    const button = document.getElementById("button");
+    console.log(button)
+}
